bd2b3aaf92f48cecadb0f089efb00d961e541427
tkgstrator <nasawake.am@gmail.com>
2022-07-05 18:33:39 +0900

    - システム情報取得に対応

--- src/api/system/system.controller.ts
+++ src/api/system/system.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { SystemService } from './system.service';
 
 @Controller('system')
-export class SystemController {}
+export class SystemController {
+  constructor(private readonly service: SystemService) {}
+
+  @Get()
+  @ApiTags('システム')
+  @ApiOperation({ operationId: 'サーバー情報' })
+  find() {
+    return this.service.getSystemInfo();
+  }
+}
--- src/api/system/system.service.ts
+++ src/api/system/system.service.ts
@@ -1,4 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import FormData from 'form-data';
+import { plainToClass } from 'class-transformer';
+import snakecaseKeys from 'snakecase-keys';
+
+export class SystemResponseDto {
+  ACTION: string;
+  DATA: SystemResponseDataDto;
+  LONGVIEW: SystemResponseViewDto;
+  NOTIFICATIONS: string[];
+  VERSION: string;
+}
+
+class SystemResponseViewDto {
+  STEP: number;
+  UPDATEDELAY: number;
+}
+
+class SystemResponseValueDto {
+  x: number;
+  y: number;
+}
+
+class SystemResponseCPUDto {
+  system: SystemResponseValueDto[];
+  user: SystemResponseValueDto[];
+  wait: SystemResponseValueDto[];
+}
+
+class SystemResponseMemoryDto {
+  buffers?: SystemResponseValueDto[];
+  cache?: SystemResponseValueDto[];
+  free: SystemResponseValueDto[];
+  used: SystemResponseValueDto[];
+}
+
+class SystemResponseDataDto {
+  CPU: {
+    cpu0: SystemResponseCPUDto;
+  };
+  Load: SystemResponseValueDto;
+  Memory: {
+    real: SystemResponseMemoryDto;
+    swap: SystemResponseMemoryDto;
+  };
+  SysInfo: {
+    arch: string;
+    client: string;
+    cpu: {
+      cores: number;
+      type: string;
+    };
+    hostname: string;
+    kernel: string;
+    os: {
+      distversion: string;
+      dist: string;
+    };
+    type: string;
+  };
+  Uptime;
+  number;
+}
 
 @Injectable()
-export class SystemService {}
+export class SystemService {
+  constructor(private readonly axios: HttpService) {}
+
+  async getSystemInfo() {
+    const url = 'https://longview.linode.com/fetch';
+    const body = new FormData();
+    body.append('api_key', process.env.API_KEY);
+    body.append('api_action', 'getLatestValue');
+    body.append(
+      'keys',
+      '["CPU.*","Disk.*","Load.*","Memory.*","Network.*","SysInfo.*","Uptime"]'
+    );
+    const response = await lastValueFrom(this.axios.post(url, body));
+
+    switch (response.status) {
+      case 400:
+        throw new BadRequestException();
+      case 404:
+        throw new NotFoundException();
+      case 403:
+        throw new ForbiddenException();
+      default:
+        return snakecaseKeys(response.data[0])
+        return response.data.map((value) =>
+          plainToClass(SystemResponseDto, value)
+        );
+    }
+  }
+}
