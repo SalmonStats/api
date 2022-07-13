aa37acd1305b444940536b565d70d65d7043e086
tkgstrator <nasawake.am@gmail.com>
2022-07-14 01:12:29 +0900

    - Add publicity flag

--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -7,21 +7,21 @@ export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(request: UserCreateInputDto) {
-    // return this.prisma.user.upsert({
-    //   where: {
-    //     uid: request.uid,
-    //   },
-    //   create: {
-    //     uid: request.uid,
-    //     name: request.display_name,
-    //     screenName: request.screen_name,
-    //     thumbnailURL: request.photoURL,
-    //   },
-    //   update: {
-    //     name: request.display_name,
-    //     screenName: request.screen_name,
-    //     thumbnailURL: request.photoURL,
-    //   },
-    // });
+    return this.prisma.user.upsert({
+      where: {
+        uid: request.uid,
+      },
+      create: {
+        uid: request.uid,
+        name: request.display_name,
+        screenName: request.screen_name,
+        thumbnailURL: request.photoURL,
+      },
+      update: {
+        name: request.display_name,
+        screenName: request.screen_name,
+        thumbnailURL: request.photoURL,
+      },
+    });
   }
 }
